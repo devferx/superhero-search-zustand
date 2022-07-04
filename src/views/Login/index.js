3512d3edb50727ee00be9c360da3e00b7fc7835d
devferx <ferkki@hotmail.com>
2022-07-03 21:40:14 -0400

    :bug: [ADD] Devtools function for superheroStore debugging

--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -34,6 +34,7 @@ export default function Login() {
     if (successSubmitted && !isSubmitting) {
       history.push("/search");
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [successSubmitted, isSubmitting]);
 
   useEffect(() => {
--- src/zustand/superhero-store.js
+++ src/zustand/superhero-store.js
@@ -1,25 +1,28 @@
 import create from "zustand";
+import { devtools } from "zustand/middleware";
 import apiCall from "./api";
 
-const superheroStore = create((set) => ({
-  isFetchingSuperheroes: false,
-  fetchSuperheroesError: undefined,
-  superheroes: [],
-  fetchSuperheroes: async (searchText) => {
-    try {
-      set({
-        fetchSuperheroesError: undefined,
-        superheroes: [],
-        isFetchingSuperheroes: true,
-      });
-      const { data } = await apiCall.get(`/search/${searchText}`);
-      set({ superheroes: data?.results });
-    } catch (error) {
-      set({ fetchSuperheroesError: error });
-    } finally {
-      set({ isFetchingSuperheroes: false });
-    }
-  },
-}));
+const superheroStore = create(
+  devtools((set) => ({
+    isFetchingSuperheroes: false,
+    fetchSuperheroesError: undefined,
+    superheroes: [],
+    fetchSuperheroes: async (searchText) => {
+      try {
+        set({
+          fetchSuperheroesError: undefined,
+          superheroes: [],
+          isFetchingSuperheroes: true,
+        });
+        const { data } = await apiCall.get(`/search/${searchText}`);
+        set({ superheroes: data?.results });
+      } catch (error) {
+        set({ fetchSuperheroesError: error });
+      } finally {
+        set({ isFetchingSuperheroes: false });
+      }
+    },
+  }))
+);
 
 export default superheroStore;
